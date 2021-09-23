450ad3a6b69918077943afab65da2642ca4e3606
MitchelCarreon <60196056+MitchelCarreon@users.noreply.github.com>
2021-09-22 21:25:32 -0500

    Removed TODOs

--- app/src/main/java/com/ualr/simpletasklist/MainActivity.java
+++ app/src/main/java/com/ualr/simpletasklist/MainActivity.java
@@ -15,8 +15,6 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    // [DONE] TODO 05. Add a TaskList member to the MainActivity. Initialize the new member.
     private final TaskList taskList = new TaskList(new HashMap<Integer, Task>());
 
     @Override
@@ -25,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //TODO 06.02 Bind the onAddBtnClicked method to the add button, so the onAddBtnClicked is
-        // triggered whenever the user clicks on that button
 
         this.binding.addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //TODO 07.02 Bind the onDeleteBtnClicked method to the delete button, so that method is
-        // triggered whenever the user clicks on that button
 
         this.binding.deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //TODO 08.02 Bind the onDoneBtnClicked method to the done button, so the onDoneBtnClicked method is
-        // triggered whenever the user clicks on that button
 
         this.binding.clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,47 +49,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // TODO 06. Create a new functionality to add a new task using the description provided
-    //  through the text field on the top of the screen by tapping the "+" on the right
-
-    // TODO 06.01. Create a new method called onAddBtnClicked.
-
-    // TODO 06.05. Invoke TaskList class' add method to ask the TaskList to
-    //  add a new Task with the description provided through the text field.
-
-    // TODO 06.06. Use TaskList class' toString method to get a string with the formatted task list
-    //  and display it on screen in the TextView with the id "textView"
-
-
     private void onAddBtnCLicked() {
         this.taskList.add(this.binding.editTextTextPersonName.getText().toString());
         this.binding.taskList.setText(this.taskList.toString());
     }
 
-    // TODO 07. Create a new functionality to delete a task from the task list
-
-    // TODO 07.01. Create a new method called onDeleteBtnClicked.
-
-    // TODO 07.04. Invoke TaskList class' delete method to ask the TaskList to
-    //  delete a Task given the id provided through the text field on the bottom.
-
-    // TODO 07.05. Use TaskList class' toString method to get a string with the formatted task list
-    //  and display it on screen in the TextView with the id "textView"
 
     private void onDeleteBtnClicked() {
         this.taskList.delete(Integer.parseInt(this.binding.editTextTaskId.getText().toString()));
         this.binding.taskList.setText(this.taskList.toString());
     }
 
-    // TODO 08. Create a new functionality to mark a task as done.
-
-    // TODO 08.01. Create a new method called onDoneBtnClicked
-
-    // TODO 08.04. Invoke TaskList class' markDone method to ask the TaskList to
-    //  mark a Task given the id provided through the text field on the bottom.
-
-    // TODO 08.05. Use TaskList class' toString method to get a string with the formatted task list
-    //  and display it on screen in the TextView with the id "textView"
 
     private void onDoneBtnClicked() {
         this.taskList.markDone(Integer.parseInt(this.binding.editTextTaskId.getText().toString()));
--- app/src/main/java/com/ualr/simpletasklist/model/Task.java
+++ app/src/main/java/com/ualr/simpletasklist/model/Task.java
@@ -5,10 +5,6 @@ public class Task {
     private String taskDesc;
     private boolean taskDone;
 
-    // [DONE ]TODO 01. Define two attributes for the Task class: one to store the task description and a second one that
-    //  indicates whether the task is done or not
-
-// [DONE] TODO 02. Define the class constructor and the corresponding getters and setters.
 
     public Task(String taskDesc, boolean taskDone) {
         this.taskDesc = taskDesc;
